cb88c28222f40b69f227c661e8b52f8f2515f134
Iggy <igoririanto@rocketmail.com>
2018-06-25 13:35:23 -0500

    add basic call/2 response plus header

--- lib/pluggie.ex
+++ lib/pluggie.ex
@@ -1,2 +1,13 @@
 defmodule Pluggie do
+  def init(default_opts) do
+    IO.puts "starting up Helloplug..."
+    default_opts
+  end
+
+  def call(conn, _opts) do
+    IO.puts "sayin hello!"
+#    Plug.Conn.send_resp(conn, 200, "Hello world!")
+    conn = Plug.Conn.put_resp_header(conn, "Server", "Pluggie")
+    Plug.Conn.send_resp(conn, 200, "Hello WOrld")
+  end
 end
