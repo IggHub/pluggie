b3208dfb115643ae78cbe5cc58635f08ec6fd8bf
Iggy <igoririanto@rocketmail.com>
2018-06-25 18:05:02 -0500

    add elixir templates and optimized so it renders only once

--- lib/pluggie.ex
+++ lib/pluggie.ex
@@ -1,22 +1,14 @@
 defmodule Pluggie do
-  def init(default_opts) do
-    IO.puts "starting up Helloplug..."
-    default_opts
-  end
+  use Router
+  require EEx
 
-  def call(conn, _opts) do
-    route(conn.method, conn.path_info, conn) 
-  end
-
-  def route("GET", ["hello"], conn) do
-    conn |> Plug.Conn.send_resp(200, "HELLO world!")
-  end
-
-  def route("GET", ["users", user_id], conn) do
-    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
-  end
+	EEx.function_from_file :defp, :template_show_user, "templates/show_user.eex", [:user_id]
+	def route("GET", ["users", user_id], conn) do
+		page_contents = template_show_user(user_id)
+		conn |> Plug.Conn.put_resp_content_type("text/html") |> Plug.Conn.send_resp(200, page_contents)
+	end
 
   def route(_method, _path, conn) do
-    conn |> Plug.Conn.send_resp(404, "Couldn't find that path_info")
+    conn |> Plug.Conn.send_resp(404, "Couldn't find the path_info")
   end
 end
