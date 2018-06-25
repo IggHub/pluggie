3f07e1476ccea56f149aa8a82f77b1626eef1715
Iggy <igoririanto@rocketmail.com>
2018-06-25 13:14:11 -0500

    installed deps

--- lib/pluggie.ex
+++ lib/pluggie.ex
@@ -1,18 +1,2 @@
 defmodule Pluggie do
-  @moduledoc """
-  Documentation for Pluggie.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pluggie.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
